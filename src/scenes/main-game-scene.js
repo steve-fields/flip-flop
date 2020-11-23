0c4c1eba616b0cf384141f51950fb71d85209c76
Breanna Tran <breannatran@gmail.com>
2020-11-22 21:55:39 -0800

    Preload and locate 'earth' asset

--- src/scenes/main-game-scene.js
+++ src/scenes/main-game-scene.js
@@ -2,17 +2,15 @@ import { Scene } from 'phaser';
 
 export default class MainGameScene extends Scene {
   preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('sky', 'http://labs.phaser.io/assets/skies/space3.png');
+    this.load.image('green', 'http://labs.phaser.io/assets/particles/green.png');
+    this.load.image('earth', '../../../assets/earthPNG.png');
   }
 
   create() {
     this.add.image(400, 300, 'sky');
 
-    const particles = this.add.particles('red');
+    const particles = this.add.particles('green');
 
     const emitter = particles.createEmitter({
       speed: 100,
@@ -20,12 +18,14 @@ export default class MainGameScene extends Scene {
       blendMode: 'ADD',
     });
 
-    const logo = this.physics.add.image(400, 100, 'logo');
 
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    const earth = this.physics.add.image(400, 100, 'earth');
 
-    emitter.startFollow(logo);
+    earth.setScale(1.2);
+    earth.setVelocity(100, 200);
+    earth.setBounce(1, 1);
+    earth.setCollideWorldBounds(true);
+
+    emitter.startFollow(earth);
   }
 }
